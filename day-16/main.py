983b021cd9823caba4af2bac92624107f6326a84
vickydarlinn <uttapalsangwan@gmail.com>
2024-05-04 22:53:06 +0530

    day 16 completed

---
+++ day-16/main.py
@@ -0,0 +1,19 @@
+# from turtle import  Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+#
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+#
+#
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+print(table)
